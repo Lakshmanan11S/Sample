1a9780b1f5160d72f5986d200ea542f3a9fb26c3
unknown <lakshmanan@elonnativesystem.com>
2024-03-15 19:31:43 +0530

    gs

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
-
-
-const router = require("./route/route.js")
-
-
 const PORT = 8000;
 const app = express();
 
@@ -14,14 +5,14 @@ app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect("mongodb://127.0.0.1:27017/employee")
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("Failed to connect to MongoDB:", err));
+    // .then(() => console.log("Connected to MongoDB"))
+    // .catch(err => console.error("Failed to connect to MongoDB:", err));
 
 app.get("/", (req, res) => {
     res.send("My name is sundhar");
 });
 
-app.use("/",router);
+app.use("/api",router);
 
 app.listen(PORT, () => {
     console.log("The server is running on PORT", PORT);
--- route/route.js
+++ route/route.js
@@ -7,7 +7,7 @@ const UserController = require('../controler/controller')
 
 router.post('/employee', UserController.create);
 
-
+router.delete('/delete/{employeeName}', UserController.delete);
 router.get('/getAll', UserController.getAll);
 
 module.exports = router
